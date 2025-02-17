16428effb88b2eda27a49e56f9044d9646c8d2ff
Dimas Wisnu Wirawan <101412302+dimaswi@users.noreply.github.com>
2025-02-17 10:33:17 +0700

    update
    - perbaikan layout untuk margin left tidak sejajar untuk semua item dan menambahkan text-align

--- resources/views/tables/columns/keuangan/jurnal-umum/second-coa-column.blade.php
+++ resources/views/tables/columns/keuangan/jurnal-umum/second-coa-column.blade.php
@@ -2,11 +2,11 @@
     @if ($getRecord()->secondary_coa == $getRecord()->primary_coa)
     @else
         @if ($getRecord()->kredit == 0)
-        <p style="font-size: 18px; margin-left:-120%">
+        <p style="font-size: 18px; text-align: start; margin-left: -200px">
             <span style="color: green; font-size: 20px">&UpArrow;</span> {{ $getState() }}
         </p>
         @else
-            <p style="font-size: 18px; margin-left:-120%">
+            <p style="font-size: 18px; text-align: start; margin-left: -200px">
                 <span style="color: red; font-size: 20px">&DownArrow;</span> {{ $getState() }}
             </p>
         @endif
