230652ab0ec85ff2ae213034c28c9beb7a181589
Dimas Wisnu Wirawan <dimasw347@gmail.com>
2025-02-17 10:25:32 +0700

    update
    - layout untuk nama akun coa menjorok ke dalam

--- resources/views/tables/columns/keuangan/jurnal-umum/second-coa-column.blade.php
+++ resources/views/tables/columns/keuangan/jurnal-umum/second-coa-column.blade.php
@@ -2,7 +2,7 @@
     @if ($getRecord()->secondary_coa == $getRecord()->primary_coa)
     @else
         @if ($getRecord()->kredit == 0)
-        <p style="font-size: 18px; margin-left:-280px">
+        <p style="font-size: 18px; margin-left:-200px">
             <span style="color: green; font-size: 20px">&UpArrow;</span> {{ $getState() }}
         </p>
         @else
